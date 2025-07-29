3c2ebf3fc59601ce069932738335df074a493b2b
shivsablok27 <shivsablok27@gmail.com>
2025-07-29 13:35:03 +0530

    version mismatch error solve tryig

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
-model = load_model("best_model.h5", compile=False)
+model = None  # <- Global reference
 
 class_info = {
     0: 'Cardboard',
@@ -14,35 +14,21 @@ class_info = {
 }
 
 def predict_trash(image_path):
-    # Load the image
+    global model
+    if model is None:
+        model = load_model("best_model.h5", compile=False)
+
     img = image.load_img(image_path, target_size=(224, 224))
-    
-    # Convert the image to an array
     img_array = image.img_to_array(img)
-    
-    # Expand dimensions to match model input shape
     img_array = np.expand_dims(img_array, axis=0)
-    
-    # Normalize the image array
     img_array /= 255.0
-    
-    # Make prediction
+
     predictions = model.predict(img_array)
-    
-    # Get the class index with the highest probability
     predicted_class_index = np.argmax(predictions[0])
-    
-    # Get the class name
     predicted_class_name = class_info[predicted_class_index]
+    confidence_score = float(round(predictions[0][predicted_class_index] * 100, 2))
 
-    # Get the confidence score
-    confidence_score = (predictions[0][predicted_class_index])* 100  # Convert to percentage
-    confidence_score = float(round(confidence_score, 2))
-
-    # Return the result as a dictionary
     return {
         'class': predicted_class_name,
         'confidence': confidence_score
     }
-    
-
